1350d0f65be64cea77d44d63f840d907f57fc8fb
Medina <medinahuseynova@hotmail.com>
2025-01-14 13:59:13 +0400

    Initial Commit

---
+++ imageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  imageFeed
+//
+//  Created by Medina Huseynova on 14.01.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
